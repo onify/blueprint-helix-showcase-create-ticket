c72a213fc51257340c4b79e77df21b0fcd47dd2c
Robert Lundsten <robert@onify.co>
2023-05-31 11:09:26 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# blueprint-helix-showcase-create-ticket
+![Onify Blueprints](https://files.readme.io/8ba3f14-onify-blueprints-logo.png)
+
+[![Project Status: WIP – Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.](https://www.repostatus.org/badges/latest/wip.svg)](https://www.repostatus.org/#wip)
+
+# Onify Blueprint: Helix Showcase - Create ticket
+
+....
+
+![Onify Blueprint: Helix Showcase - Create ticket](blueprint.jpg "Blueprint")
+
+## Requirements
+
+* [Onify Hub](https://github.com/onify/install)
+* [Onify Helix App (custom)](https://github.com/onify/helix-app-boilerplate)
+
+## Setup
+
+### Setup Workflow
+
+1. Copy `./hub/resources/flows/create-ticket.bpmn` to `./resources/flows` 
+2. Sync resources with Git if needed
+3. Create a new Workflow with key `create-ticket` and set `bpmnresource` to `create-ticket.bpmn`
+
+### Setup Create ticket form
+
+1. Copy ...
+2. Add to `pages.yml`
+...
+
+## Support
+
+* Community/forum: https://support.onify.co/discuss
+* Documentation: https://support.onify.co/docs
+* Support and SLA: https://support.onify.co/docs/get-support
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- helix/src/custom/pages/create-ticket.vue
+++ helix/src/custom/pages/create-ticket.vue
@@ -1,93 +1,113 @@
 <template>
-  <o-page-container-forms>
-    <v-form @submit.prevent="onSubmit">
-      <v-col class="create-ticket text-left">
-        <div class="text-h6 mb-2">Contact: {{ userSettingsStore?.userSettings?.name }}</div>
-        <v-text-field
-          v-model="form.title"
-          label="Title"
-          hint="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-          persistent-hint
-        ></v-text-field>
-        <v-textarea
-          v-model="form.subject"
-          label="Subject"
-          hint="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-          persistent-hint
-        ></v-textarea>
-        <o-file-upload
-          v-model="form.attachments"
-          label="Attachments"
-          hint="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-          persistent-hint
-        ></o-file-upload>
-        <v-btn class="mt-9" color="primary" size="large" type="submit">Submit</v-btn>
-      </v-col>
+  <o-page-container type="form">
+    <v-text-field
+      v-model="user.name"
+      class="mb-3"
+      label="Contact"
+      hint="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      prepend-icon="mdi:mdi-account"
+      persistent-hint
+      disabled
+    />
+    <v-form ref="vFormRef" validate-on="input" @submit.prevent="onSubmit">
+      <v-text-field
+        v-model="formData.title"
+        class="mb-3"
+        label="Title"
+        prepend-icon="mdi:mdi-form-textbox"
+        hint="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        :rules="[validate.required, validate.minLength(5)]"
+        persistent-hint
+      />
+      <v-textarea
+        v-model="formData.subject"
+        class="mb-3"
+        label="Subject"
+        prepend-icon="mdi:mdi-text"
+        hint="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        :rules="[validate.required, validate.minLength(20)]"
+        persistent-hint
+      />
+      <v-file-input
+        v-model="files"
+        label="Attachments"
+        class="mb-3"
+        hint="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        persistent-hint
+        multiple
+        accept="image/png, image/jpeg, .png, .jpg, .jpeg .doc, .docx, .xml,application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        show-size
+        :rules="[validate.maxLength(3), validate.maxFileSize(30_000, 10_000)]"
+      />
+      <v-btn class="mt-9" color="primary" type="submit" :loading="isSubmitting">Submit</v-btn>
     </v-form>
-  </o-page-container-forms>
+  </o-page-container>
 </template>
 
 <script setup>
-import { OFileUpload, OPageContainerForms } from '@onify/helix-components';
-import { useUserSettingsStore, useOnifyApi, useAlerts } from '@onify/helix-core';
-import { reactive } from 'vue';
+const { alerts, user, onifyApiRequest } = usePageCommon();
 
-const userSettingsStore = useUserSettingsStore();
-const onifyHttpRequest = useOnifyApi();
-const alerts = useAlerts();
+/*
+  Vue Template refs: https://vuejs.org/guide/essentials/template-refs.html
+  We do this to access vForm's `validate()` function
+*/
+const vFormRef = ref();
 
-const CREATE_TICKET_WORKFLOW_URL = 'my/workflows/run/create-ticket?timeout=60';
+// Form data to submit to the api
+const formData = reactive({});
 
-const form = reactive({
-  title: '',
-  subject: '',
-  attachments: [],
-  contact: '',
-});
+// Ref variable is used for the loading effect for the submit button
+const isSubmitting = ref(false);
+
+// Files array
+const files = ref([]);
 
 async function onSubmit() {
-  try {
-    const fileUploadResults = await onifyHttpRequest.upload('/files/public/', form.attachments);
-    form.attachments = fileUploadResults;
-    const response = await onifyHttpRequest.update(CREATE_TICKET_WORKFLOW_URL, { json: form });
+  // Make sure to validate the before prior to submission
+  const { valid } = await vFormRef.value.validate();
 
-    if (response.status === 200) {
-      // result.json() returns a promise, so we run an `await` to get the response body
-      const createTicketResult = await response.json();
+  if (valid) {
+    try {
+      isSubmitting.value = true;
 
-      alerts.addAlert({
-        type: 'success',
-        title: 'Ticket Submission Sucessful',
-        body: `Ticket number: ${createTicketResult.output.ticketNumber}`,
+      // Upload files to Onify API
+      const fileUploadResults = await onifyApiRequest.uploadFiles({
+        type: 'public',
+        files: files.value,
+        ttl: 3000,
+        role: ['admin', 'manager'],
+        refresh: false,
+        tag: ['image', 'jpg'],
       });
+      // Assign uploaded files to `formData.attachments`
+      formData.attachments = fileUploadResults;
+
+      // Submit the form. Sends a `POST` request to the API with the form data as payload
+      const response = await onifyApiRequest('my/workflows/run/create-ticket?timeout=60', { method: 'post', json: formData }).response();
+
+      // Submission attempt completed. Removes the loading effect on the button
+      isSubmitting.value = false;
+
+      // Resets the form
+      vFormRef.value.reset();
+
+      // If `response.status` is 200(OK) submission was successful and we can present the ticket number to the user
+      if (response.status === HTTPStatusCode.OK) {
+        // result.json() returns a promise, so we run an `await` to get the response body
+        const createTicketResult = await response.json();
+
+        // Show "Submission Successful" alert
+        alerts.addAlert({
+          type: 'success',
+          title: 'Ticket Submission Successful',
+          body: `Ticket number: ${createTicketResult.output.ticketNumber}`,
+        });
+      }
+    } catch (err) {
+      /*  An error occurred somewhere during the form submission attempt.
+          Remove the loading effect on the submit button to allow user to attempt to submit again */
+      isSubmitting.value = false;
     }
-  } catch (err) {
-    alerts.addAlert({
-      type: 'error',
-      title: 'Ticket Submission Failed',
-      body: 'Please try again',
-      details: {
-        title: err.name,
-        description: err.stack,
-        isCode: true,
-      },
-      actions: [
-        {
-          text: 'Show Details',
-          willShowDetailsOnClick: true,
-        },
-      ],
-    });
   }
 }
 </script>
-
-<style>
-.create-ticket {
-  max-width: 587px;
-}
-
-.create-ticket > * {
-  margin-bottom: 12px;
-}
-</style>
